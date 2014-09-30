690b33ddd149e39268cef114fdd6479a09d1c3b9
Jack You <gmyou71@gmail.com>
2014-09-30 16:58:35 +0900

    Update deleteNews.py

--- deleteNews.py
+++ deleteNews.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import _mysql
-import sys
-import date
+from datetime import date, datetime, timedelta
+import MySQLdb
 
-theday = date.today()
-print(theday)
+cnx = MySQLdb.connect('localhost', 'user', 'pwd', 'test')
+cursor = cnx.cursor()
+theMonth = datetime.now().date() + timedelta(months=-1)
+add_employee = ("delete from naver_rank_news where cr_date=%s")
+data_employee = (theMonth)
+# Delete
+cursor.execute(add_employee, data_employee)
 
-try:
-    con = _mysql.connect('localhost', 'user', 'password', 'database')
-    print ("delete from naver_rank_news where cr_date='"+theday+"'")
-    con.query("delete from naver_rank_news where cr_date='"+theday+"'")
-    result = con.use_result()
-
-    print "MySQL version: %s" % \
-        result.fetch_row()[0]
-
-except _mysql.Error, e:
-
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
-
-finally:
-
-    if con:
-        con.close()
+cnx.commit()
+cursor.close()
+cnx.close()
